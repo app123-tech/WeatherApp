da3a42b1557b96c851e350eb5155e16e5e49f060
app123-tech <appdeveloper662@gmail.com>
2025-02-03 13:36:09 +0545

    initial

--- app/src/main/java/com/appdevelopers/weatherapp/MainActivity.java
+++ app/src/main/java/com/appdevelopers/weatherapp/MainActivity.java
@@ -5,6 +5,13 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.appdevelopers.weatherapp.Fragment.FragmentTenDays;
+import com.appdevelopers.weatherapp.Fragment.FragmentTodayActivity;
+import com.appdevelopers.weatherapp.Fragment.FragmentTomorrow;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonToday, buttonTomorrow, buttonTenDays;
@@ -14,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        buttonToday = findViewById(R.id.buttonToday);
+        buttonTomorrow = findViewById(R.id.buttonTomorrow);
+        buttonTenDays = findViewById(R.id.buttonTenDays);
+
+        buttonToday.setOnClickListener(v -> {
+            loadFragment(new FragmentTodayActivity());
+        });
+
+        buttonTomorrow.setOnClickListener(v -> {
+            loadFragment(new FragmentTomorrow());
+        });
+
+        buttonTenDays.setOnClickListener(v -> {
+            loadFragment(new FragmentTenDays());
+        });
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
