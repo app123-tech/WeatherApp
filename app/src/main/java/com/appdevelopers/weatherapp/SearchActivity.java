039d9176a39f215f0ad4a0820db13e75eb755765
app123-tech <appdeveloper662@gmail.com>
2025-02-03 17:26:15 +0545

    initial

--- app/src/main/java/com/appdevelopers/weatherapp/SearchActivity.java
+++ app/src/main/java/com/appdevelopers/weatherapp/SearchActivity.java
@@ -1,14 +1,14 @@
 package com.appdevelopers.weatherapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SearchActivity extends AppCompatActivity {
+    private TextView noLocationFound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +16,16 @@ public class SearchActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search);
 
+        noLocationFound = findViewById(R.id.noLocationFound);
+
+        handleSearchResult(false);
+    }
+
+    private void handleSearchResult(boolean isLocationFound) {
+        if (isLocationFound) {
+            noLocationFound.setVisibility(View.GONE);
+        } else {
+            noLocationFound.setVisibility(View.VISIBLE);
+        }
     }
 }
