78eac9501b3eeb0d34371a8715eaee83240b5e69
app123-tech <appdeveloper662@gmail.com>
2025-02-07 16:14:54 +0545

    initial

--- app/src/main/java/com/appdevelopers/weatherapp/PrivacyPolicy.java
+++ app/src/main/java/com/appdevelopers/weatherapp/PrivacyPolicy.java
@@ -10,17 +10,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TermsAndConditions extends AppCompatActivity {
-    private WebView termsAndConditionsWebView;
+public class PrivacyPolicy extends AppCompatActivity {
+    private WebView privacyPolicyWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_terms_and_conditions);
-
-        termsAndConditionsWebView = findViewById(R.id.termsAndConditionsWebView);
-        termsAndConditionsWebView.setWebViewClient(new WebViewClient());
-        termsAndConditionsWebView.loadUrl("D:\\Android\\WeatherApp\\app\\src\\main\\assets\\terms.html");
+        setContentView(R.layout.activity_privacy_policy);
+       privacyPolicyWebView = findViewById(R.id.privacyPolicyWebView);
+       privacyPolicyWebView.setWebViewClient(new WebViewClient());
+       privacyPolicyWebView.loadUrl("D:\\Android\\WeatherApp\\app\\src\\main\\assets\\terms.html");
     }
 }
