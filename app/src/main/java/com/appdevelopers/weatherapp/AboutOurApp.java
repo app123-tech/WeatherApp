310517b03ec0591c850303266a6d31f0dd57f3a1
app123-tech <appdeveloper662@gmail.com>
2025-02-07 14:47:46 +0545

    initial

---
+++ app/src/main/java/com/appdevelopers/weatherapp/AboutOurApp.java
@@ -0,0 +1,52 @@
+package com.appdevelopers.weatherapp;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.internal.GsonBuildConfig;
+
+
+public class AboutOurApp extends AppCompatActivity {
+    private TextView textViewVersion, textViewLatestVersionInstallMessage;
+    private ImageView imageViewBackArrow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about_our_app);
+
+        textViewVersion = findViewById(R.id.textViewVersion);
+        imageViewBackArrow = findViewById(R.id.imageViewBackArrow);
+        textViewLatestVersionInstallMessage = findViewById(R.id.textViewLatestVersionInstallMessage);
+
+        updateVersionNumber();
+
+        imageViewBackArrow.setOnClickListener(v -> onBackPressed());
+    }
+
+    public void updateVersionNumber(){
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+            textViewVersion.setText("Version " + versionName);
+
+            String latestVersion = "1.0";
+            if (versionName.equals(latestVersion)) {
+                textViewLatestVersionInstallMessage.setText("The latest version is already installed");
+            } else {
+                textViewLatestVersionInstallMessage.setText("A new version is available");
+            }
+        } catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+            textViewVersion.setText("Version info not available");
+            textViewLatestVersionInstallMessage.setText("Version Information Error");
+        }
+    }
+}
--- app/src/main/java/com/appdevelopers/weatherapp/SplashScreenActivity.java
+++ app/src/main/java/com/appdevelopers/weatherapp/SplashScreenActivity.java
@@ -22,6 +22,6 @@ public class SplashScreenActivity extends AppCompatActivity {
             Intent intent = new Intent(SplashScreenActivity.this, GetStartedActivity.class);
             startActivity(intent);
             finish();
-        }, 2000);
+        }, 1000);
     }
 }
