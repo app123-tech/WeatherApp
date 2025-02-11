be6dce8f0ac5121d10ba362adce347e3e34dad23
app123-tech <appdeveloper662@gmail.com>
2025-02-11 17:23:56 +0545

    initial

--- app/src/main/java/com/appdevelopers/weatherapp/Fragment/FragmentTomorrow.java
+++ app/src/main/java/com/appdevelopers/weatherapp/Fragment/FragmentTomorrow.java
@@ -7,11 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.appdevelopers.weatherapp.R;
 
 
 public class FragmentTomorrow extends Fragment {
+    private TextView textViewWind, textViewWindSecond, textViewWindSpeed, textViewAQI, textViewAQISpeed, textViewDirection, textViewDirectionSpeed, textViewHumidity, textViewHumidityValue, textViewHumidityContent;
+    private TextView textViewHourlyForecast, textViewHourlyForecastNow, textViewHourlyForecast10AM, textViewHourlyForecast11AM, textViewHourlyForecast12PM, textViewHourlyForecast1PM, textViewHourlyForecast2PM, textViewTempNow, textViewTemp10AM, textViewTemp11AM, textViewTemp12PM, textViewTemp1PM, textViewTemp2PM;
+    private TextView textViewSunriseTime, textViewSunsetTime;
+    private ImageView imageViewHourlyForecastNow, imageViewHourlyForecast10AM, imageViewHourlyForecast11AM, imageViewHourlyForecast12PM, imageViewHourlyForecast1PM, imageViewHourlyForecast2PM;
+
 
 
     public FragmentTomorrow() {
